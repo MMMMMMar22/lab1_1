7cc8fdb8eaf9f63d058441bc0923085eda4f1e24
Mariya <klever.vanlav@gmail.com>
2025-11-03 18:36:42 +0500

    Добавлен алгоритм Евклида

---
+++ main.py
@@ -0,0 +1,9 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
+result = gcd(num1, num2)
+print(f"НОД чисел {num1} и {num2} равен: {result}")
